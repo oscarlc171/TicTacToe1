316755310d50395ae5e434bd2789bc1a83c3af92
oscarlc171 <oscarrcollazo@gmail.com>
2022-06-14 19:42:30 -0600

    TicTacToe1

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -11,40 +11,82 @@ namespace ConsoleApp5
 
             void ShowGameBoard()
             {
-                Console.WriteLine("-----------");
+                Console.WriteLine("-------------------");
                 Console.WriteLine($"{gameboard[0, 0]}|{gameboard[0, 1]}|{gameboard[0, 2]}\n" +
                         $"------\n" +
                         $"{gameboard[1, 0]}|{gameboard[1, 1]}|{gameboard[1, 2]}\n" +
                         $"------\n" +
                         $"{gameboard[2, 0]}|{gameboard[2, 1]}|{gameboard[2, 2]}");
-                Console.WriteLine("-----------");
+                Console.WriteLine("-------------------");
             }
 
             Console.WriteLine("Game Rules \n" +
                 "----------\n" +
                 "Choose a coordinate for where you want to mark your move.\n" +
-                "Type in the first number(from 0-2), press Enter, then type in your second number(from 0-2)");
+                "Type in the first number for Column(from 1-3), press Enter, then type in your second number for Row(from 1-3) and press Enter again");
+
+            Console.WriteLine("-------------------");
+            Console.WriteLine("Columns\n" +
+                "1 2 3\n" +
+                "| | |\n" +
+                "v v v");
+            Console.WriteLine($"{gameboard[0, 0]}|{gameboard[0, 1]}|{gameboard[0, 2]} <---1\n" +
+                    $"------\n" +
+                    $"{gameboard[1, 0]}|{gameboard[1, 1]}|{gameboard[1, 2]} <---2 Rows\n" +
+                    $"------\n" +
+                    $"{gameboard[2, 0]}|{gameboard[2, 1]}|{gameboard[2, 2]} <---3");
+            Console.WriteLine("-------------------");
 
             while (gameboard[0, 0] == 0 || gameboard[0, 1] == 0 || gameboard[0, 2] == 0 ||
                 gameboard[1, 0] == 0 || gameboard[1, 1] == 0 || gameboard[1, 2] == 0 ||
                 gameboard[2, 0] == 0 || gameboard[2, 1] == 0 || gameboard[2, 2] == 0)
             {
                 Console.WriteLine("Player 1's turn");
-                ShowGameBoard();
 
 
                 int Player1FirstNumber = int.Parse(Console.ReadLine());
                 int Player1SecondNumber = int.Parse(Console.ReadLine());
 
-                while (Player1FirstNumber > 2 || Player1FirstNumber < 0 || Player1SecondNumber > 2 || Player1SecondNumber < 0)
+                while (Player1FirstNumber > 3 || Player1FirstNumber < 1 || Player1SecondNumber > 3 || Player1SecondNumber < 1)
                 {
+                    Console.Clear();
                     Console.WriteLine("Enter a valid coordinate");
+                    ShowGameBoard();
                     Player1FirstNumber = int.Parse(Console.ReadLine());
                     Player1SecondNumber = int.Parse(Console.ReadLine());
                 }
 
-                gameboard[Player1FirstNumber, Player1SecondNumber] = 1;
+                while (gameboard[Player1SecondNumber - 1, Player1FirstNumber - 1] != 0)
+                {
+                    Console.Clear();
+                    Console.WriteLine("You can't make a move here. You must choose an empty space.");
+                    ShowGameBoard();
+                    Player1FirstNumber = int.Parse(Console.ReadLine());
+                    Player1SecondNumber = int.Parse(Console.ReadLine());
+                }
 
+                gameboard[Player1SecondNumber - 1, Player1FirstNumber - 1] = 1;
+
+                if ((gameboard[0,0] == 1 && gameboard[0,1] == 1 && gameboard[0,2] == 1) || (gameboard[1,0] == 1 &&
+                    gameboard[1,1] == 1 && gameboard[1,2] == 1) || (gameboard[2,0] == 1 && gameboard[2,1] == 1 &&
+                    gameboard[2,2] == 1) || (gameboard[0,0] == 1 && gameboard[1,0] == 1 && gameboard[2,0] == 1) ||
+                    (gameboard[0,1] == 1 && gameboard[1,1] == 1 && gameboard[2,1] == 1) || (gameboard[0,2] == 1 &&
+                    gameboard[1,2] == 1 && gameboard[2,2] == 1) || (gameboard[0,0] == 1 && gameboard[1,1] == 1 &&
+                    gameboard[2,2] == 1) || (gameboard[0,2] == 1 && gameboard[1,1] == 1 && gameboard[2,0] == 1))
+                {
+                    Console.WriteLine("Player 1 wins");
+                    break;
+                }
+
+                if (gameboard[0, 0] != 0 && gameboard[0, 1] != 0 && gameboard[0, 2] != 0 &&
+                gameboard[1, 0] != 0 && gameboard[1, 1] != 0 && gameboard[1, 2] != 0 &&
+                gameboard[2, 0] != 0 && gameboard[2, 1] != 0 && gameboard[2, 2] != 0)
+                {
+                    Console.WriteLine("Draw");
+                    break;
+                }
+
+                Console.Clear();
 
                 Console.WriteLine("Player 2's turn");
                 ShowGameBoard();
@@ -53,21 +95,47 @@ namespace ConsoleApp5
                 int Player2FirstNumber = int.Parse(Console.ReadLine());
                 int Player2SecondNumber = int.Parse(Console.ReadLine());
 
-                while (Player2FirstNumber > 2 || Player2FirstNumber < 0 || Player2SecondNumber > 2 || Player2SecondNumber < 0)
+                while (Player1FirstNumber > 3 || Player1FirstNumber < 1 || Player1SecondNumber > 3 || Player1SecondNumber < 1)
                 {
+                    Console.Clear();
                     Console.WriteLine("Enter a valid coordinate");
-                    Player2FirstNumber = int.Parse(Console.ReadLine());
-                    Player2SecondNumber = int.Parse(Console.ReadLine());
+                    ShowGameBoard();
+                    Player1FirstNumber = int.Parse(Console.ReadLine());
+                    Player1SecondNumber = int.Parse(Console.ReadLine());
                 }
 
-                gameboard[Player2FirstNumber, Player2SecondNumber] = 2;
+                while (gameboard[Player1SecondNumber - 1, Player1FirstNumber - 1] != 0)
+                {
+                    Console.Clear();
+                    Console.WriteLine("You can't make a move here. You must choose an empty space.");
+                    ShowGameBoard();
+                    Player1FirstNumber = int.Parse(Console.ReadLine());
+                    Player1SecondNumber = int.Parse(Console.ReadLine());
+                }
+
+                gameboard[Player2SecondNumber - 1, Player2FirstNumber - 1] = 2;
+
+                Console.Clear();
+
                 ShowGameBoard();
+
+                if ((gameboard[0, 0] == 2 && gameboard[0, 1] == 2 && gameboard[0, 2] == 2) || (gameboard[1, 0] == 2 &&
+                    gameboard[1, 1] == 2 && gameboard[1, 2] == 2) || (gameboard[2, 0] == 2 && gameboard[2, 1] == 2 &&
+                    gameboard[2, 2] == 2) || (gameboard[0, 0] == 2 && gameboard[1, 0] == 2 && gameboard[2, 0] == 2) ||
+                    (gameboard[0, 1] == 2 && gameboard[1, 1] == 2 && gameboard[2, 1] == 2) || (gameboard[0, 2] == 2 &&
+                    gameboard[1, 2] == 2 && gameboard[2, 2] == 2) || (gameboard[0, 0] == 2 && gameboard[1, 1] == 2 &&
+                    gameboard[2, 2] == 2) || (gameboard[0, 2] == 2 && gameboard[1, 1] == 2 && gameboard[2, 0] == 2))
+                {
+                    Console.WriteLine("Player 2 wins");
+                    break;
+                }
 
 
                 if (gameboard[0, 0] != 0 && gameboard[0, 1] != 0 && gameboard[0, 2] != 0 &&
                 gameboard[1, 0] != 0 && gameboard[1, 1] != 0 && gameboard[1, 2] != 0 &&
                 gameboard[2, 0] != 0 && gameboard[2, 1] != 0 && gameboard[2, 2] != 0)
                 {
+                    Console.WriteLine("Draw");
                     break;
                 }
             }
